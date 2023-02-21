9c0e320416e951f1977c96e536e20c5b07751795
Pravinjayavarman Asogan <PRAJA@orsted.com>
2023-02-21 17:50:36 +0800

    Project

---
+++ TFSCICD/CICDPoc/FeatureFiles/POC.feature
@@ -0,0 +1,9 @@
+﻿Feature: POC
+	In order to ensure app installation is done in site
+	As a Deca tester
+	I want to ensure Decafying App is installed
+
+
+@mytag
+Scenario: Verify Google is launched
+	Given I have launched test site 
---
+++ TFSCICD/CICDPoc/Steps/POCSteps.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium.Interactions;
+using System.Collections.Generic;
+using System.Configuration;
+using DecaAutomationFramework;
+using SharedHelperClass;
+
+namespace CICDPoc.Steps
+{
+    [Binding]
+    public class POCSteps
+    {
+        IWebDriver driver;
+
+        String decafyLibraryLabel;
+
+        [Given(@"I have launched test site")]
+        public void GivenIHaveLaunchedTestSite()
+        {
+            //Functions._Login(driver, url);
+
+            driver = new ChromeDriver();
+            //Functions._Login(driver, "");
+            driver.Manage().Cookies.DeleteAllCookies(); //delete all cookies
+            Thread.Sleep(5000); //wait 5 seconds to clear cookies.
+            driver.Manage().Window.Maximize();
+            Thread.Sleep(5000);
+            driver.Navigate().GoToUrl("https://www.google.com");
+            Thread.Sleep(40000);
+
+        }
+
+
+
+ 
+
+
+
+
+     
+    }
+}
